2557b6b24a125d8e1c2777a38208d6ebdfcb6cfa
SferrellaA <alexander@sferrella.com>
2024-02-07 22:02:18 -0500

    still trying to get things in a way I like

---
+++ content/index.py
@@ -0,0 +1,4 @@
+from browser import document, html
+for i in range (1,10):
+    document <= str(i)
+    document <= html.BR()
--- server.py
+++ server.py
@@ -1,24 +1,54 @@
 from bottle import route, run
-#        <script type=<"text/javascript" src="/brython.js"></script>
+from pathlib import Path
 
-x = '''\
+brython = open("brython.js").read()
+@route('/brython.js')
+def serveBrython():
+    return brython
+
+html = '''\
 <html>
     <head>
         <meta charset="utf-8">
-        <script src="https://cdn.jsdelivr.net/npm/brython@3.12.1/brython.min.js"></script>
+        <script src="/brython.js"></script>
         <title>{title}</title>
     </head>
-    <body onload="brython()">
-        <script type="text/python">
-{script}
-        </script
-    </body>
+    <body onload="brython()">{content}</body>
 </html>\
 '''
 
+def getPath(path):
+    p = Path(path)
+    if p.is_file():
+        return open(path).read()
+    if p.is_dir():
+        if path[-1] != '/':
+            path += '/'
+        if Path(path + "index.html").is_file():
+            return open(path + "index.html").read()
+        if Path(path + "index.py").is_file():
+            return open(path + "index.py").read()
+    else:
+        return "Not Found"
 
+def enscript(script):
+    return f"<script type=\"text/python\">{getPath(script)}</script>"
+
+def call(script):
+    return f"<script type=\"text/python\" src=\"{script}\"></script>"
+
+'''
 @route('/index.py')
 def hello():
     return x.format(title="index.py", script=open("index.py").read())
+'''
+
+x = "/index.py"
+@route(x)
+def main():
+    return html.format(title=x, content=call(x))
 
 run(host='localhost', port=8080, debug=True)
+
+
+
